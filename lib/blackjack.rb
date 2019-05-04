a4781b63c5cae8576b5b5a740349801409a08848
rudi hinds <rudihinds@gmail.com>
2019-05-04 21:21:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,10 +49,14 @@ end
 
 
 def runner
-  welcome
-  total = initial_round
-  hit?(number)
-  get_user_input
+  # code runner here
+  welcome  
+  card_total = initial_round
+  while card_total < 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
+  end
+  end_game(card_total)
 
 end
     
